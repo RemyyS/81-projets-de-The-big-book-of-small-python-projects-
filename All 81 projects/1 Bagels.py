f5c66938fc8125101edd46073aa5ca9de96ffc8d
Remy <severinremy@hotmail.fr>
2024-10-09 11:30:22 +0200

    updated code, added test file, almost finished prototyping

--- All 81 projects/1 Bagels.py
+++ All 81 projects/1 Bagels.py
@@ -6,16 +6,65 @@
 # You have 10 tries to guess the secret number.
 
 import pyinputplus as pyip
-
-numberdemande = 0
-
-while len(str(numberdemande)) > 3 or len(str(numberdemande)) < 3:
-    numberdemande = pyip.inputNum("input 3 numbers>")
-    if len(str(numberdemande)) > 3 or len(str(numberdemande)) < 3:
-        print ("Those are not 3 numbers")
+def main():
+    print ("game start")
 
 
 
+sizeofinputnumber = 3 #The size of the number you want to play with
+numbertoguess = 0
+numberofguesses = 10 #Number of guesses allowed to the user
+guess = 0
+listfermipico = [] #List of "Fermi" and "Pico" that has to be randomized before being returned to not textualy give away the place of where the clue is obtained
+replayagain = ""
+sizeofinputnumber = pyip.inputNum("Enter the size of number you want to play with (default = 3)>")
+
+while len(str(numbertoguess)) > sizeofinputnumber or len(str(numbertoguess)) < sizeofinputnumber:
+    numbertoguess = pyip.inputNum(f"Input {sizeofinputnumber} numbers to guess>")
+    if len(str(numbertoguess)) > sizeofinputnumber or len(str(numbertoguess)) < sizeofinputnumber:
+        print (f"Those are not {sizeofinputnumber} numbers. Try again.")
 
 
-print (numberdemande)
+print ("Try to guess the number.\n The program will give you a hint:\n "
+       "\"Pico\" if you have a correct digit in the wrong place\n "
+       "\"Fermi\" if you have a correct digit in the correct place\n "
+       "\"Bagels\" if your guess has no correct digits")
+
+
+while len(str(guess)) > sizeofinputnumber or len(str(guess)) < sizeofinputnumber:
+    guess = pyip.inputNum(f"Input {sizeofinputnumber} numbers as guess, you have {numberofguesses} guesses left >")
+    if len(str(guess)) > sizeofinputnumber or len(str(guess)) < sizeofinputnumber:
+        print ("Those are not 3 numbers. Try again.")
+
+
+while guess != numbertoguess:
+    for i in range(len(str(guess))):
+        if str(guess)[i] == str(numbertoguess)[i]:
+            listfermipico.append("Fermi") #A good number is in the correct place
+        elif str(guess)[i] in str(numbertoguess):
+            listfermipico.append("Pico") #A good number is not at the right place
+        elif len(listfermipico) == 0:
+            print ("Bagels flop!")
+    listfermipico = listfermipico.sort() 
+    print (listfermipico)
+    while len(str(guess)) > sizeofinputnumber or len(str(guess)) < sizeofinputnumber:
+        guess = pyip.inputNum(f"Input {sizeofinputnumber} numbers as guess, you have {numberofguesses} guesses left >")
+        if len(str(guess)) > sizeofinputnumber or len(str(guess)) < sizeofinputnumber:
+            print ("Those are not 3 numbers. Try again.")
+    
+                
+
+
+if numberofguesses == 0:
+    print (f"No more guesses left ! The answer was {numbertoguess}")
+
+if guess == numbertoguess:
+    replayagain = pyip.inputYesNo("Congratulations ! Would you like to play again ?")
+    if replayagain == "yes":
+        main()
+
+if __name__ == '__main__':
+    main()
+
+
+print (guess)
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 81 practice programs from the book of AL SWEITGART "The Big Book of Small Python Projects"
 
 1 : Bagels
+
 2 : Birthday Paradox
